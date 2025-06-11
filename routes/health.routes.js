166f41397aaedbb3e0f73923f6862c1408f05bbe
nsurname2038 <214315071+nsurname2038@users.noreply.github.com>
2025-06-11 11:47:24 +0300

    feat: add `rate limiting` middleware (#13)

--- routes/health.routes.js
+++ routes/health.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
+import rateLimiters from "../middleware/rate-limit.middleware.js";
 
 import healthController from "../controllers/health.controller.js";
 
 const router = express.Router();
 
-router.get("/", healthController.getHealthStatus);
+router.get("/", rateLimiters.default, healthController.getHealthStatus);
 
 export default router;
