3b785845f591f157a2f66b866ab3bec044e20261
nsurname2038 <214315071+nsurname2038@users.noreply.github.com>
2025-06-11 11:36:45 +0300

    feat: add `Prisma ORM` for database operations (#12)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import app from "./app.js";
 import config from "./config/index.config.js";
 import logger from "./helpers/logger.helper.js";
 
+import postgres from "./database/postgres.database.js";
 import redis from "./cache/redis.cache.js";
 
 async function start() {
@@ -11,6 +12,7 @@ async function start() {
     const port = config.server.port;
 
     await redis.connect();
+    await postgres.connect();
 
     app.listen(port, () => {
       logger.info(`Server is running at http://${host}:${port}.`);
